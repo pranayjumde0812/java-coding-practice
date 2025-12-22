37029a1acbb1eeea34564f7df625b147efafbd2d
Pranay Jumde <pranay.jumde@qodequay.com>
2025-12-22 18:46:28 +0530

    find longest prefix

---
+++ interview-coding-question/src/LongestCommonPrefix.java
@@ -0,0 +1,51 @@
+/**
+ * Find Longest Common prefix from String array.
+ */
+
+public class LongestCommonPrefix {
+
+    public static String findLongestCommonPrefix1(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if (prefix.isEmpty())
+                    return "";
+
+            }
+        }
+
+        return prefix;
+    }
+
+    public static String longestCommonPrefix2(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+
+            for (int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
+
+    public static void main(String[] args) {
+        String[] arr = {"fly", "flight", "flower"};
+
+        System.out.println(findLongestCommonPrefix1(arr));
+
+        System.out.println("===========================");
+
+        System.out.println(longestCommonPrefix2(arr));
+    }
+}
